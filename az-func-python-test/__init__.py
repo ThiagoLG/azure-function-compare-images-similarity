ca54a12dd5a4d880ce019f6915016e2dea261baf
Thiago Lourençon Ghebra <thiago,lg53@gmail.com>
2023-01-25 01:33:11 -0300

    Adjusting init file, improving structure (ifs and else)

--- az-func-python-test/__init__.py
+++ az-func-python-test/__init__.py
@@ -1,5 +1,3 @@
-import logging
-
 import azure.functions as func
 import base64
 import numpy as np
@@ -28,74 +26,46 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             status_code=400
         )
 
-    # processamento do documento 1
-    if doc1:
-        try:
-            file1 = ''
-        except ValueError:
-            return func.HttpResponse(
-                'Não foi possível processar o documento evniado.',
-                status_code=400
-            )
-    else:
+    if doc1 and doc2:
+        # leitura das imagens recebidas
+        original = readb64(doc1)
+        image_to_compare = readb64(doc2)
+
+        # realiza a leitura da imagem e obtém pontos chave para comparação
+        sift = cv2.xfeatures2d.SIFT_create()
+        kp_1, desc_1 = sift.detectAndCompute(original, None)
+        kp_2, desc_2 = sift.detectAndCompute(image_to_compare, None)
+
+        # parâmetros par comparação
+        index_params = dict(algorithm=0, trees=5)
+        search_params = dict()
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(desc_1, desc_2, k=2)
+
+        # array para armazenar pontos de semelhança
+        good_points = []
+
+        # percorre todas as correspondências entre as imagens para calcular a semelhança
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good_points.append(m)
+
+        # calcula a quantidade de pontos de semelhança
+        number_keypoints = 0
+        if len(kp_1) <= len(kp_2):
+            number_keypoints = len(kp_1)
+        else:
+            number_keypoints = len(kp_2)
+
+        percentage = len(good_points) / number_keypoints * 100
+
+        # retorna a porcentagem de semelhança -
         return func.HttpResponse(
-            'Documento 1 não recebido',
+            f"{percentage}",
             status_code=200
         )
-
-    # processamento do documento 2
-    if doc2:
-        try:
-            file2 = ''
-        except ValueError:
-            return func.HttpResponse(
-                'Não foi possível processar o documento enviado.',
-                status_code=400
-            )
     else:
-        return func.HttpResponse(
-            'Documento 2 não recebido',
+         return func.HttpResponse(
+            "Problema na leitura dos documentos enviados, por favor, confira os dados e tente novamente",
             status_code=200
         )
-
-    # leitura das imagens recebidas
-    original_img = readb64(doc1)
-    image_to_compare_img = readb64(doc2)
-
-    # converte para grayscale
-    original = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
-    image_to_compare = cv2.cvtColor(image_to_compare_img, cv2.COLOR_BGR2GRAY)
-
-    # realiza a leitura da imagem e obtém pontos chave para comparação
-    sift = cv2.xfeatures2d.SIFT_create()
-    kp_1, desc_1 = sift.detectAndCompute(original, None)
-    kp_2, desc_2 = sift.detectAndCompute(image_to_compare, None)
-
-    # parâmetros par comparação
-    index_params = dict(algorithm=0, trees=5)
-    search_params = dict()
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
-    matches = flann.knnMatch(desc_1, desc_2, k=2)
-
-    # array para armazenar pontos de semelhança
-    good_points = []
-
-    # percorre todas as correspondências entre as imagens para calcular a semelhança
-    for m, n in matches:
-        if m.distance < 0.7 * n.distance:
-            good_points.append(m)
-
-    # calcula a quantidade de pontos de semelhança
-    number_keypoints = 0
-    if len(kp_1) <= len(kp_2):
-        number_keypoints = len(kp_1)
-    else:
-        number_keypoints = len(kp_2)
-
-    percentage = len(good_points) / number_keypoints * 100
-
-    # retorna a porcentagem de semelhança -
-    return func.HttpResponse(
-        f"Percentual de semelhança: {percentage}%",
-        status_code=200
-    )
